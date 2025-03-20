577b6c2fbd930b04a7a76580e8b66fd565a8e31c
YuheshPandian <yuheshpandian@gmail.com>
2025-03-20 11:05:49 +0530

    Learned about datatypes

---
+++ code/main.py
@@ -0,0 +1,52 @@
+# A variable is a box that can hold a value.
+# for example
+box = "balls"
+name = "Yuhesh"
+age = 15
+
+# so from the above example we can learn that a variable can have diff values.
+# each variety is called as a datatype
+
+# Different Data types
+#string
+"yuhesh"
+'pandian'
+'''HEy there this is
+just 🔥''' # multiline comment
+
+
+#int
+1
+2
+3
+
+# float
+
+3.3
+1.0
+33.33
+pi = 3.141
+root3 = 1.732
+
+# boolean
+
+True or False
+# int form of true is 1 and int form of false is 0
+
+# list
+[1,2,3,4,5,6] # mutable, can have duplicate values
+
+# tuple
+(1,2,4,65) # immutable
+
+# set
+{20,4,2,5} # mutable , can't have duplicates
+
+# dictionary
+# It is in key value pair
+{"2":"two", "one":1}
+
+
+# You can check the type of a datatype with help of following code snippet
+
+print(type(name)) # -> gives <class 'str'>
